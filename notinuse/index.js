d600abc59d2145163271a7c9c3b7020dc69669d8
Kobraz <roar.f.hanssen@gmail.com>
2023-02-16 21:52:47 +0100

    div

---
+++ notinuse/index.js
@@ -0,0 +1,19 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c718b2dfccmsh0cbd42aac7fb08ep110b48jsnc0168724acac',
+		'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
+	}
+};
+
+fetch('https://car-data.p.rapidapi.com/cars?make=Volvo', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+console.log(response);
+		
+const cars = document.getElementById("cars");
+
+cars.innerHTML += "<li><a href="#">Car ${cars.children.length + 1}</a></li>";
+
