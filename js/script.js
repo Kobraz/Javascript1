173cf9760d691b2fe79aa2bce0e9936c9fb86ffc
Kobraz <roar.f.hanssen@gmail.com>
2023-02-17 09:18:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ fetch("https://fakestoreapi.com/products").then((data)=>{
     console.log(objectData[0].title);
     let tableData="";
     objectData.map((values)=>{
-        tableData+=`<tr><td>${values.title}</td><td>${values.description}</td><td>${values.price}</td><td><img src=${values.image}></td></tr>`;
+        tableData+=` <tr>
+        <td>${values.title}</td>
+        <td>${values.description}</td>
+        <td>${values.price}</td>
+        <td><img src="${values.image}"></td>
+        </tr>`;
     });
     document.getElementById("table_body").innerHTML=tableData;
 }).catch((err)=>{
