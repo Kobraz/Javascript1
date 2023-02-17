863cc53b68f1294dd7dd94690dabbaf0622220b1
Kobraz <roar.f.hanssen@gmail.com>
2023-02-17 11:20:05 +0100

    Update cars.js

--- js/cars.js
+++ js/cars.js
@@ -1,4 +1,12 @@
-fetch("https://carapi.app").then((data)=>{
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c718b2dfccmsh0cbd42aac7fb08ep110b48jsnc0168724acac',
+		'X-RapidAPI-Host': 'car-api2.p.rapidapi.com'
+	}
+};
+
+fetch("https://car-api2.p.rapidapi.com/api/years", options).then((data)=>{
     // console.log(data); json format
     return data.json(); //converted to object
 }).then((objectData)=>{
@@ -6,7 +14,7 @@ fetch("https://carapi.app").then((data)=>{
     let tableData="";
     objectData.map((values)=>{
         tableData+=` <tr>
-        <td>${values.title}</td>
+        <td>${values.make}</td>
         <td>${values.description}</td>
         <td>${values.price}</td>
         <td><img src="${values.image}"></td>
