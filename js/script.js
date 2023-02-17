19f49b779b001fbb89d4294e00c4bad663572e00
Kobraz <roar.f.hanssen@gmail.com>
2023-02-17 22:48:18 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 fetch("https://fakestoreapi.com/products").then((data)=>{
-    // console.log(data); json format
-    return data.json(); //converted to object
+    
+    return data.json();
 }).then((objectData)=>{
     console.log(objectData[0].title);
     let tableData="";
