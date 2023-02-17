57376518ff2fcdcd7ddde8b3df5f03d1ace1352f
Kobraz <roar.f.hanssen@gmail.com>
2023-02-17 10:13:17 +0100

    html js

---
+++ js/cars.js
@@ -0,0 +1,19 @@
+fetch("https://carapi.app").then((data)=>{
+    // console.log(data); json format
+    return data.json(); //converted to object
+}).then((objectData)=>{
+    console.log(objectData[0].title);
+    let tableData="";
+    objectData.map((values)=>{
+        tableData+=` <tr>
+        <td>${values.title}</td>
+        <td>${values.description}</td>
+        <td>${values.price}</td>
+        <td><img src="${values.image}"></td>
+        </tr>`;
+    });
+    document.getElementById("tableBody").innerHTML=tableData;
+}).catch((err)=>{
+    console.log(err);
+})
+
