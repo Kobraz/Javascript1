9e7ff5c3b60a55b05ac38979a50d57540c0c33f8
Kobraz <roar.f.hanssen@gmail.com>
2023-02-18 15:03:04 +0100

    js html

--- js/validation.js
+++ js/validation.js
@@ -1,12 +1,33 @@
-validateEmail = (input) => {
-    var mailformat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if(input.value.match(mailformat))
-    {
-        const emailError = document.getElementById('email-error');
-        emailError.textContent = "Valid email address!";
+// validateEmail = (input) => {
+//     var mailformat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+//     if(input.value.match(mailformat))
+//     {
+//         const emailError = document.getElementById('email-error');
+//         emailError.textContent = "Valid email address!";
+//     }
+//     else {
+//         const emailError = document.getElementById('email-error');
+//         emailError.textContent = "Email is not valid!";
+//     }
+// }
+
+
+function validation() {
+    var form = document.getElementById("form");
+    var form = document.getElementById("email").value;
+    var form = document.getElementById("text");
+    var pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if (email.match(pattern)) {
+        form.classList.add("Valid");
+        form.classList.remove("Invalid");
+        Text.innerHTML = "Valid email"
+        Text.style.color = "#00ff00";
     }
     else {
-        const emailError = document.getElementById('email-error');
-        emailError.textContent = "Email is not valid!";
+        form.classList.add("Invalid");
+        form.classList.remove("Valid");
+        Text.innerHTML = "Invalid email"
+        Text.style.color = "#ff0000";
     }
 }
