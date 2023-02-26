308efa56f37303e2fad2aeace3fadd5984b2b6b3
Kobraz <roar.f.hanssen@gmail.com>
2023-02-26 22:44:03 +0100

    Update details.js

--- js/details.js
+++ js/details.js
@@ -21,11 +21,13 @@ async function getDetail() {
     imageContainer.innerHTML += `<img src="${data.image}">`;
 }
 
-try {
-    console.log(data.title);
-} catch (exc) {
-    alert("Error");
-}
+throw new Error("An unexpected error occurred");
+
+// try {
+//     console.log(data.title);
+// } catch (exc) {
+//     alert("Error");
+// }
 
 
 getDetail();
