4db0bbd661655bfb496548ec0730fc3fbe09e0bd
Kobraz <roar.f.hanssen@gmail.com>
2023-03-11 16:27:51 +0100

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -2,9 +2,9 @@ window.addEventListener("load", () => {
     
     const loader = document.querySelector(".loading");
     loader.classList.add("loading-hidden");
-    loader.addEventListener("transitionend", () => {
-        // document.body.removeChild("loading");
+    // loader.addEventListener("transitionend", () => {
+    //     document.body.removeChild("loading");
 
-    })
+    // })
 
 })
