a33dc9e5f6eb6530aa41c7abca8e755cbc240726
Kobraz <roar.f.hanssen@gmail.com>
2023-03-12 12:29:19 +0100

    Update validation2.js

--- js/validation2.js
+++ js/validation2.js
@@ -1,9 +1,4 @@
-var nameIsValid = 0;
-var subjIsValid = 0;
-var mailIsValid = 0;
-var addrIsValid = 0;
-
-function mailValidation(mailIsValid) {
+function mailValidation() {
     var email = document.getElementById("email").value;
     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
@@ -13,7 +8,6 @@ function mailValidation(mailIsValid) {
         emailtextok.innerHTML = " &#x2714;";
         emailtextok.style.color = "#00ff00";
         emailtext.innerHTML = "";
-        mailIsValid = 1;
     }
     else {
         form.classList.add("Invalid");
@@ -21,7 +15,6 @@ function mailValidation(mailIsValid) {
         emailtext.innerHTML = "Email must be valid format";
         emailtext.style.color = "#ff3333";
         emailtextok.innerHTML = "";
-        mailIsValid = 0;
     }
 }
 
@@ -34,13 +27,11 @@ function nameValidation() {
         nametextok.innerHTML = " &#x2714;";
         nametextok.style.color = "#00ff00";
         nametext.innerHTML = "";
-        nameIsValid = 1;
     }
     else {
         nametext.innerHTML = "Name required";
         nametext.style.color = "#ff3333";
         nametextok.innerHTML = "";
-        nameIsValid = 0;
     }
 }
 
@@ -53,13 +44,11 @@ function subjValidation() {
         subjtextok.innerHTML = " &#x2714;";
         subjtextok.style.color = "#00ff00";
         subjtext.innerHTML = "";
-        subjIsValid = 1;
     }
     else {
         subjtext.innerHTML = "Subject must be 10 chars";
         subjtext.style.color = "#ff3333";
         subjtextok.innerHTML = "";
-        subjIsValid = 0;
     }
 }
 
@@ -72,13 +61,11 @@ function addrValidation() {
         addrtextok.innerHTML = " &#x2714;";
         addrtextok.style.color = "#00ff00";
         addrtext.innerHTML = "";
-        addrIsValid = 1;
     }
     else {
         addrtext.innerHTML = "Address must be 25 chars";
         addrtext.style.color = "#ff3333";
         addrtextok.innerHTML = "";
-        addrIsValid = 0;
     }
 }
 
@@ -86,21 +73,8 @@ const form = document.getElementById('form');
 form.addEventListener('submit', () => {
     nameValidation();
     subjValidation();
-    mailValidation(mailIsValid);
+    mailValidation();
     addrValidation();
 })
 
-var isValid = nameIsValid + subjIsValid + mailIsValid + addrIsValid;
 
-if(isValid === 4) {
-    document.getElementById("submitButton").style.display = "true";
-}
-else {
-    document.getElementById("submitButton").style.display = "none";
-}
-
-console.log("Name " + nameIsValid);
-console.log("Subj " + subjIsValid);
-console.log("Mail " + mailIsValid);
-console.log("Addr " + addrIsValid);
-console.log("Total " + isValid);
