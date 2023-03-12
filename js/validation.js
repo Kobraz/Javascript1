891e45597c194c6f8512c8fe7814b5b6f71feaba
Kobraz <roar.f.hanssen@gmail.com>
2023-03-12 15:30:01 +0100

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -13,7 +13,7 @@ function mailValidation() {
         form.classList.add("Invalid");
         form.classList.remove("Valid");
         emailtext.innerHTML = "Email must be valid format";
-        emailtext.style.color = "#6e0000";
+        emailtext.style.color = "#d50000";
         emailtextok.innerHTML = "";
     }
 }
@@ -30,7 +30,7 @@ function nameValidation() {
     }
     else {
         nametext.innerHTML = "Name required";
-        nametext.style.color = "#6e0000";
+        nametext.style.color = "#d50000";
         nametextok.innerHTML = "";
     }
 }
@@ -47,7 +47,7 @@ function subjValidation() {
     }
     else {
         subjtext.innerHTML = "Subject must be 10 chars";
-        subjtext.style.color = "#6e0000";
+        subjtext.style.color = "#d50000";
         subjtextok.innerHTML = "";
     }
 }
@@ -64,7 +64,7 @@ function addrValidation() {
     }
     else {
         addrtext.innerHTML = "Address must be 25 chars";
-        addrtext.style.color = "#6e0000";
+        addrtext.style.color = "#d50000";
         addrtextok.innerHTML = "";
     }
 }
